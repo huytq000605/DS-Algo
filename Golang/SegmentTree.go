56e1e2ed62835201f7b79c7b05d67c61b1e9080f
HuyTQ <ygoldendarkness@gmail.com>
2021-06-16 03:03:46 +0700

    Segment Tree

---
+++ Golang/SegmentTree.go
@@ -0,0 +1,75 @@
+package SegmentTree
+
+import "math"
+
+type SegmentTree struct {
+	// range
+	Start int
+	End   int
+
+	//value
+	Maximum int
+	Lazy    int
+
+	//Tree
+	Left  *SegmentTree
+	Right *SegmentTree
+}
+
+func (this *SegmentTree) Query(s, e int) int {
+	if s < this.Start || e > this.End {
+		return math.MinInt32
+	}
+
+	if s >= this.Start && e <= this.End {
+		return this.Maximum
+	}
+
+	this.normalize()
+	leftQuery := this.Left.Query(s, e)
+	rightQuery := this.Right.Query(s, e)
+	if leftQuery > rightQuery {
+		return leftQuery
+	} else {
+		return rightQuery
+	}
+}
+
+func (this *SegmentTree) Update(s, e, val int) {
+	if s < this.Start || e > this.End {
+		return
+	}
+
+	if s >= this.Start && e <= this.End {
+		this.Maximum += val
+		this.Lazy += val
+		return
+	}
+
+	this.normalize()
+	this.Left.Update(s, e, val)
+	this.Right.Update(s, e, val)
+
+	if this.Left.Maximum > this.Right.Maximum {
+		this.Maximum = this.Left.Maximum
+	} else {
+		this.Maximum = this.Right.Maximum
+	}
+}
+
+func (this *SegmentTree) normalize() {
+	if this.Start != this.End {
+		if this.Left == nil || this.Right == nil {
+			middle := this.Start + (this.End-this.Start)/2
+			this.Left = &SegmentTree{Start: this.Start, End: middle, Maximum: this.Maximum}
+			this.Right = &SegmentTree{Start: middle + 1, End: this.End, Maximum: this.Maximum}
+		}
+		if this.Lazy != 0 {
+			this.Left.Maximum += this.Lazy
+			this.Left.Lazy += this.Lazy
+			this.Right.Maximum += this.Lazy
+			this.Right.Lazy += this.Lazy
+		}
+	}
+	this.Lazy = 0
+}
---
+++ Typescript/SegmentTree.ts
@@ -0,0 +1,61 @@
+class SegmentTree {
+    left: SegmentTree;
+    right: SegmentTree;
+    start: number;
+    end: number;
+    k: number;
+    lazy: number;
+
+    constructor(s: number, e: number, k: number) {
+        this.start = s;
+        this.end = e;
+        this.k = k;
+    }
+
+    query(s: number, e: number): number {
+        if (s > this.end || e < this.start) {
+            return 0;
+        }
+        if (s <= this.start && e >= this.end) {
+            return this.k;
+        }
+        this.normalize();
+        const leftQuery = this.left.query(s, e);
+        const rightQuery = this.right.query(s, e);
+        return Math.max(leftQuery, rightQuery);
+    }
+
+    update(s: number, e: number, val: number) {
+        if (s > this.end || e < this.start) {
+            return;
+        }
+        if (s <= this.start && e >= this.end) {
+            this.k += val;
+            this.lazy += val;
+            return;
+        }
+        this.normalize();
+        this.left.update(s, e, val);
+        this.right.update(s, e, val);
+        this.k = Math.max(this.left.k, this.right.k);
+    }
+
+    normalize() {
+        if (this.start < this.end) {
+            // not leaf node
+            if (!this.left || !this.right) {
+                const middle =
+                    this.start + Math.floor((this.end - this.start) / 2);
+                this.left = new SegmentTree(this.start, middle, this.k);
+                this.right = new SegmentTree(middle + 1, this.end, this.k);
+            }
+			if (this.lazy) {
+                this.left.k += this.lazy;
+                this.left.lazy += this.lazy;
+                this.right.k += this.lazy;
+                this.right.lazy += this.lazy;
+            }
+        }
+        this.lazy = 0;
+    }
+}
