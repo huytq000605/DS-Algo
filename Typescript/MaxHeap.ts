297bcae8cefb7baa9bb69cb4067f2bc458c64277
HuyTQ <ygoldendarkness@gmail.com>
2021-06-19 03:30:32 +0700

    Heap

--- Typescript/MaxHeap.ts
+++ Typescript/MaxHeap.ts
@@ -1,28 +1,38 @@
+// class HeapNode {
+//     word: string;
+//     freq: number;
+// }
+
+type HeapNode = number
+
 class MaxHeap {
-    private heap: number[];
+    private heap: HeapNode[];
     private _length: number;
     constructor() {
         this.heap = [];
         this._length = 0;
     }
 
-	get length() {
-		return this._length
-	}
-	
-    public peek() {
-		if(this._length > 0)
-			return this.heap[0];
-		else 
-			return undefined
+    get length() {
+        return this._length;
     }
-    public push(val: number) {
+
+    public peek() {
+        if (this._length > 0) return this.heap[0];
+        else return undefined;
+    }
+
+    public push(val: HeapNode) {
         this.heap.push(val);
         this._length++;
         this.bubbleUp();
     }
+
     public pop() {
-        [this.heap[0], this.heap[this._length - 1]] = [this.heap[this._length - 1],this.heap[0]]
+        [this.heap[0], this.heap[this._length - 1]] = [
+            this.heap[this._length - 1],
+            this.heap[0],
+        ];
         const pop = this.heap.pop();
         this._length--;
         this.bubbleDown();
@@ -32,14 +42,18 @@ class MaxHeap {
         let current = this._length - 1;
         while (current > 0) {
             let parent = Math.ceil(current / 2) - 1;
-            if (this.heap[current] > this.heap[parent]) {
-				[this.heap[current], this.heap[parent]] = [ this.heap[parent], this.heap[current]];
+            if (this.compare(this.heap[current], this.heap[parent]) == 1) {
+                [this.heap[current], this.heap[parent]] = [
+                    this.heap[parent],
+                    this.heap[current],
+                ];
                 current = parent;
             } else {
                 return;
             }
         }
     }
+	
     private bubbleDown() {
         let current = 0;
         while (true) {
@@ -48,12 +62,12 @@ class MaxHeap {
             let right = left + 1;
             if (right >= this._length) right = left;
             let compare;
-            if (this.heap[left] > this.heap[right]) {
+            if (this.compare(this.heap[left], this.heap[right]) == 1) {
                 compare = left;
             } else {
                 compare = right;
             }
-            if (this.heap[compare] > this.heap[current]) {
+            if (this.compare(this.heap[compare], this.heap[current]) == 1) {
                 [this.heap[compare], this.heap[current]] = [
                     this.heap[current],
                     this.heap[compare],
@@ -63,5 +77,15 @@ class MaxHeap {
                 return;
             }
         }
+    }
+
+    private compare(node1: HeapNode, node2: HeapNode): number {
+        if (node1 > node2) {
+            return 1;
+        }
+        if (node1 == node1) {
+            return 0 
+        }
+        return -1;
     }
 }
--- Typescript/MinHeap.ts
+++ Typescript/MinHeap.ts
@@ -16,11 +16,13 @@ class MinHeap {
 		else 
 			return undefined
     }
+
     public push(val: number) {
         this.heap.push(val);
         this._length++;
         this.bubbleUp();
     }
+    
     public pop() {
         [this.heap[0], this.heap[this._length - 1]] = [this.heap[this._length - 1],this.heap[0]]
         const pop = this.heap.pop();
@@ -28,11 +30,12 @@ class MinHeap {
         this.bubbleDown();
         return pop;
     }
+
     private bubbleUp() {
         let current = this._length - 1;
         while (current > 0) {
             let parent = Math.ceil(current / 2) - 1;
-            if (this.heap[current] < this.heap[parent]) {
+            if (this.compare(this.heap[current], this.heap[parent]) == -1) {
 				[this.heap[current], this.heap[parent]] = [ this.heap[parent], this.heap[current]];
                 current = parent;
             } else {
@@ -40,6 +43,7 @@ class MinHeap {
             }
         }
     }
+
     private bubbleDown() {
         let current = 0;
         while (true) {
@@ -48,12 +52,12 @@ class MinHeap {
             let right = left + 1;
             if (right >= this._length) right = left;
             let compare;
-            if (this.heap[left] < this.heap[right]) {
+            if (this.compare(this.heap[left], this.heap[right]) == -1) {
                 compare = left;
             } else {
                 compare = right;
             }
-            if (this.heap[compare] < this.heap[current]) {
+            if (this.compare(this.heap[compare], this.heap[current]) == -1) {
                 [this.heap[compare], this.heap[current]] = [
                     this.heap[current],
                     this.heap[compare],
@@ -64,10 +68,14 @@ class MinHeap {
             }
         }
     }
+
+    private compare(node1: HeapNode, node2: HeapNode): number {
+        if (node1 > node2) {
+            return 1;
+        }
+        if (node1 == node1) {
+            return 0 
+        }
+        return -1;
+    }
 }
-/**
- * Your SeatManager object will be instantiated and called as such:
- * var obj = new SeatManager(n)
- * var param_1 = obj.reserve()
- * obj.unreserve(seatNumber)
- */
