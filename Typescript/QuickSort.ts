c12c41bd78fed6a69beb7608c4956c6d4672af4b
HuyTQ <ygoldendarkness@gmail.com>
2021-06-30 13:46:07 +0700

    Quick Sort TS

---
+++ Typescript/QuickSort.ts
@@ -0,0 +1,19 @@
+function quickSort(nums: number[]): void {
+	function partition(start: number, end: number) {
+		if(start >= end) {
+			return
+		}
+		let pivot = nums[end]
+		let j = start - 1; // j is the last index where nums[j] < pivot
+		for(let i = start; i <= end; i++) {
+			if(nums[i] < pivot) {
+				j++
+				[nums[i], nums[j]] = [nums[j], nums[i]]
+			}
+		}
+		[nums[j + 1], nums[end]] = [nums[end], nums[j + 1]]
+		partition(start, j)
+		partition(j + 2, end)
+	}
+    return partition(0, nums.length - 1)
+};
